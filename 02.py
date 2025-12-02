d3558b26ac639afc8d61ee131d98622c725283c5
DejanPeric058 <najedcirep@gmail.com>
2025-12-02 06:41:54 +0100

    Day 2 part b

--- 02.py
+++ 02.py
@@ -4,14 +4,15 @@ with open("input/02.txt") as f:
 counter = 0
 commands = text.split(',')
 commands = [x.split('-') for x in commands]
-# commands = [(int(a), int(b)) for (a,b) in commands]
-print(commands)
 counter = 0
 for a, b in commands:
     ai, bi = int(a), int(b)
     for d in range(ai,bi+1):
         ds = str(d)
         n =len(ds)
-        if ds[:n//2] == ds[n//2:]:
-            counter+=d
+        for m in range(1,n//2+1):
+            res = {ds[i:i + m] for i in range(0, n, m)}
+            if len(res) == 1:
+                counter+=d
+                break
 print(counter)
