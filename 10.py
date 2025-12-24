5ab427819701fce1322514145711148e0b6ce143
DejanPeric058 <najedcirep@gmail.com>
2025-12-24 21:43:02 +0100

    Day 10 part b

--- 10.py
+++ 10.py
@@ -9,6 +9,7 @@ commands = text.split('\n')
 # print(commands)
 import itertools
 import numpy as np
+from scipy.optimize import linprog
 
 class Machine:
     def __init__(self, config):
@@ -57,37 +58,11 @@ class Machine:
         return sez
     
     def min_integer_solution(self):
-        minimum = 999999999
-        n1, m1 = len(self.buttons_matrix), len(self.buttons_matrix[0])
-        bb = np.array(self.buttons_matrix)
-        m = np.linalg.matrix_rank(bb)
-        # print(m)
-        for comb1 in itertools.combinations([i for i in range(n1)], m):
-            for comb2 in itertools.combinations([i for i in range(m1)], m):
-                # print(list(comb1),list(comb2), bb)
-                comb = bb[list(comb1),:][:,list(comb2)]
-                # print(np.linalg.det(comb),comb)
-                if np.abs(np.linalg.det(comb)) > 5.551115123125776e-10:
-                    # print(np.linalg.det(comb))
-                    comb2 = list(comb2)
-                    # print(comb, np.array(self.joltage_prepared)[comb2])
-                    x = np.linalg.solve(np.transpose(comb),np.array(self.joltage_prepared)[comb2])
-                    # x = [int(y) for y in x]
-                    # print([int(y) for y in x],[int(np.abs(y)) for y in x])
-                    # print(np.dot(np.transpose(bb[list(comb1),:]),x), np.array(self.joltage_prepared))
-                    if min(x) >= 0 and np.abs(sum(np.dot(np.transpose(bb[list(comb1),:]),x) - np.array(self.joltage_prepared))) < 5.551115123125776e-5:
-                        # print(np.abs(sum(np.dot(np.transpose(bb[list(comb1),:]),x) - np.array(self.joltage_prepared))))
-                        # print(len(np.transpose(comb)),len(np.transpose(comb)[0]),x)
-                        # print(np.dot(np.transpose(comb),x) , np.array(self.joltage_prepared)[comb2])
-                        # print(np.dot(np.transpose(bb[list(comb1),:]),x) , np.array(self.joltage_prepared))
-                        # print(np.transpose(comb),np.transpose(bb[list(comb1),:]),x)
-                        # print(len(np.transpose(bb[list(comb1),:])),len(np.transpose(bb[list(comb1),:])[0]),x)
-                        if sum([abs(t-np.round(t)) for t in x]) > 5.551115123125776e-5:
-                            print(x)
-                        z = x
-                        minimum = min(sum(x), minimum)
-        # print(z)
-        # print(minimum)
+        a = [[self.buttons_matrix[j][i] for j in range(len(self.buttons_matrix))] for i in range(len(self.buttons_matrix[0]))]
+        bounds = [(0,None) for _ in range(len(a[0]))]
+        c = [1 for _ in range(len(a[0]))]
+        res = linprog(c=c, A_eq=a,b_eq=self.joltage_prepared,bounds=bounds,integrality=1)
+        minimum = res.fun
         return minimum
     
     def calculate_min_presses(self):
@@ -109,8 +84,8 @@ for c in commands:
     list_of_machines.append(Machine(c))
 
 for i, m in enumerate(list_of_machines):
-    print('-----------------------')
-    print(i)
+    # print('-----------------------')
+    # print(i)
     counter += m.calculate_min_presses()
     counter2 += m.min_integer_solution()
     # print(counter2)
@@ -118,19 +93,3 @@ for i, m in enumerate(list_of_machines):
     # print(m.joltage_prepared)
 print(counter)
 print(counter2)
-a = [[0,0,0,1],[0,1,0,1],[0,0,1,0],[0,0,1,1],[1,0,1,0],[1,1,0,0]]
-b = np.array([3, 5, 4, 7])
-# x = np.linalg.solve(a,b)
-# print(x)
-def min_integer_solution(a,b):
-    minimum = 999999999
-    n, m = len(a), len(a[0])
-    for comb in itertools.combinations(a, m):
-        if np.linalg.det(comb) != 0:
-            x = np.linalg.solve(np.transpose(np.array(comb)),b)
-            x = [int(y) for y in x]
-            # print([int(y) for y in x],[int(np.abs(y)) for y in x])
-            if [int(y) for y in x] == [int(np.abs(y)) for y in x]:
-                minimum = min(sum(x), minimum)
-    return minimum
-# print(min_integer_solution(a,b))
